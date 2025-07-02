f4bd29143dd4959d8e400a2a0cdd0373965ec03f
ktunprasert <ktunprasert@outlook.com>
2025-07-02 19:38:58 +0100

    feat(nvimrc): add project based entries to CSpace super menu

---
+++ .nvim.lua
@@ -0,0 +1,24 @@
+local term_ops = { auto_close = false }
+
+local entries = {
+    {
+        " v:run",
+        function() Snacks.terminal(("v run %s"):format(vim.fn.expand("%")), term_ops) end,
+        "v",
+    },
+    {
+        " v:test",
+        function() Snacks.terminal(("v test %s"):format(vim.fn.expand("%:h")), term_ops) end,
+        "t",
+    },
+    {
+        "󰕒 submit",
+        function() Snacks.terminal(("exercism submit %s"):format(vim.fn.expand("%")), term_ops) end,
+        "<CR>",
+    },
+}
+
+
+for _, value in pairs(entries) do
+    Menus.append(value[1], value[2], value[3])
+end
