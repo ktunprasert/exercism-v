4671c0b64671973247a1d26ea432203b247332d2
ktunprasert <ktunprasert@outlook.com>
2025-07-03 01:58:29 +0100

    anagram: s.to_lower()

--- anagram/anagram.v
+++ anagram/anagram.v
@@ -3,6 +3,7 @@ module main
 fn find_anagrams(subject string, candidates []string) []string {
 	target := subject.to_lower().runes().sorted(a < b)
 	return candidates.filter(fn [target, subject] (s string) bool {
-		return s.to_lower() != subject.to_lower() && target == s.to_lower().runes().sorted(a < b)
+		slow := s.to_lower()
+		return slow != subject.to_lower() && target == slow.runes().sorted(a < b)
 	})
 }
